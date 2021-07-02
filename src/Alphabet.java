e45c4cb38570ff4434e544bf0cfdd7cf5ec691f6
ilimkanu <ilimkanu@gmail.com>
2021-07-02 09:06:33 +0600

    9.1

---
+++ src/Alphabet.java
@@ -0,0 +1,10 @@
+public class Alphabet {
+    public static void main(String[] args) {
+        char a = 'A';
+
+        while (a <= 'Z') {
+            System.out.println(a);
+            a++;
+        }
+    }
+}
